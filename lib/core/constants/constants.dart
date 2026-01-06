b7eab3a0fcfe17732c9cec92c4f279de9cd76fa2
SilvaRuan16 <ruan.gustavo.soares16@gmail.com>
2026-01-06 15:27:57 -0300

    update: Atualizando appbar_component.dart

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+/// CONFIGURAÇÕES DE CONSTANTES QUE SERÃO UTILIZADAS NO APP
+/// 1. FONTE SIZE
+const appBarTitleSize = 24.0;
+const textSizeSystem = 20.0;
+const iconSizeSystem = textSizeSystem;
+
+/// 2. CORES
+const Color favoriteIcon = Colors.red;
+const Color readLaterIcon = Colors.green;
--- lib/views/widgets/appbar_widget.dart
+++ lib/views/widgets/appbar_widget.dart
@@ -1,20 +1,39 @@
+import 'package:biblia_sagrada/core/constants/constants.dart';
 import 'package:flutter/material.dart';
 
 class AppbarWidget extends StatelessWidget implements PreferredSizeWidget {
-  const AppbarWidget({super.key, required this.title, this.actions});
+  const AppbarWidget({
+    super.key,
+    required this.title,
+    this.actions,
+    this.centerTitle,
+    this.automaticallyImplyLeading,
+    this.bottomTabBar,
+  });
 
   final String title;
+  final bool? centerTitle;
   final List<Widget>? actions;
+  final bool? automaticallyImplyLeading;
+  final PreferredSizeWidget? bottomTabBar;
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      title: Text(title, style: TextStyle(fontSize: 24.0)),
+      title: Text(title, style: TextStyle(fontSize: appBarTitleSize)),
       actions: [...actions ?? []],
-      centerTitle: true,
+      bottom: bottomTabBar,
+      automaticallyImplyLeading: automaticallyImplyLeading ?? true,
+      centerTitle: centerTitle ?? true,
     );
   }
 
   @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+  Size get preferredSize {
+    double height = kToolbarHeight;
+    if (bottomTabBar != null) {
+      height += bottomTabBar!.preferredSize.height;
+    }
+    return Size.fromHeight(height);
+  }
 }
