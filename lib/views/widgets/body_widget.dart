3d7f9c786533816a3685d74bae4152a0cb5507d4
SilvaRuan16 <ruan.gustavo.soares16@gmail.com>
2026-01-08 19:20:15 -0300

    update: Update file body_widget.dart and add an atribute where will checked if the content be static (using singlechildscrollview) or load an request data into listview.builder

--- lib/views/widgets/body_widget.dart
+++ lib/views/widgets/body_widget.dart
@@ -6,16 +6,19 @@ import 'package:flutter/material.dart';
 /// na tela do dispositivo.
 
 class BodyWidget extends StatelessWidget {
-  const BodyWidget({super.key, required this.child});
+  const BodyWidget({super.key, required this.child, this.hasScroll = true});
   final Widget child;
+
+  // A INTERFACE POR PADRÃO VAI TER O SINGLECHILDSCROLLVIEW PARA DEIXAR UMA BARRA DE ROLAGEM EM CONTEÚDOS COMO BOTÕES, TEXTOS ENTRE OUTROS. CASO A INTERFACE PRECISE PEGAR DADOS DE UMA REQUISIÇÃO, ELA VAI REMOVER O SINGLECHILDSCROLLVIEW PARA QUE SEJA POSSÍVEL INSERIR UMA LISTVIEW.BUILDER NO CHILD DO COMPONENTE, PARA QUE NÃO HAJA PROBLEMA DE PERFORMANCE POR CARREGAR DUAS LISTAS. ALÉM DE CONSEGUIR INSERIR UMA LISTA COM DADOS CAPTURADOS DE UMA REQUISIÇÃO.
+  final bool? hasScroll;
+
+  Widget _buildBody(BuildContext context) {
+    return SafeArea(child: Padding(padding: const EdgeInsetsGeometry.all(16.0), child: child));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsetsGeometry.all(16.0),
-          child: child,
-        ),
-    );
+    final body = _buildBody(context);
+    return hasScroll! ? SingleChildScrollView(child: body) : body;
   }
 }
